14e748fb0adc484bf45cace2870ed577f4fb3885
ANUBITO <95010732+ANUBITO@users.noreply.github.com>
2021-12-03 12:17:16 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,133 +1,236 @@
-var td1 = document.getElementById('td1');
-var td2 = document.getElementById('td2');
-var td3 = document.getElementById('td3');
-var td4 = document.getElementById('td4');
-var td5 = document.getElementById('td5');
-var td6 = document.getElementById('td6');
-var td7 = document.getElementById('td7');
-var td8 = document.getElementById('td8');
-var td9 = document.getElementById('td9');
-
-td1.addEventListener("click", td1fun);
-td2.addEventListener("click", td2fun);
-td3.addEventListener("click", td3fun);
-td4.addEventListener("click", td4fun);
-td5.addEventListener("click", td5fun);
-td6.addEventListener("click", td6fun);
-td7.addEventListener("click", td7fun);
-td8.addEventListener("click", td8fun);
-td9.addEventListener("click", td9fun);
-
-td1.style.cursor = 'pointer';
-td2.style.cursor = 'pointer';
-td3.style.cursor = 'pointer';
-td4.style.cursor = 'pointer';
-td5.style.cursor = 'pointer';
-td6.style.cursor = 'pointer';
-td7.style.cursor = 'pointer';
-td8.style.cursor = 'pointer';
-td9.style.cursor = 'pointer';
-
-var DiagonalLeftToRight = 0;
-var DiagonalRightToLeft = 0;
-var FirstLineHorizontal = 0;
-var MiddleLineHorizontal = 0;
-var LastLineHorizontal = 0;
-var FirstLineVertical = 0;
-var MiddleLineVertical = 0;
-var LastLineVertical = 0;
-
-function td1fun() {
-    DiagonalLeftToRight++;
-    FirstLineHorizontal++;
-    FirstLineVertical++;
-    td1.style.backgroundColor = 'yellow';
-    td1.removeEventListener('click', td1fun);
-    WinCheck();
-}
-
-function td2fun() {
-    FirstLineHorizontal++;
-    MiddleLineVertical++;
-    td2.style.backgroundColor = 'yellow';
-    td2.removeEventListener('click', td2fun);
-    WinCheck();
-}
-
-function td3fun() {
-    DiagonalRightToLeft++;
-    FirstLineHorizontal++;
-    LastLineVertical++;
-    td3.style.backgroundColor = 'yellow';
-    td3.removeEventListener('click', td3fun);
-    WinCheck();
-}
-
-function td4fun() {
-    FirstLineVertical++;
-    MiddleLineHorizontal++;
-    td4.style.backgroundColor = 'yellow';
-    td4.removeEventListener('click', td4fun);
-    WinCheck();
-}
-
-function td5fun() {
-    MiddleLineHorizontal++;
-    MiddleLineVertical++;
-    DiagonalRightToLeft++;
-    DiagonalLeftToRight++;
-    td5.style.backgroundColor = 'yellow';
-    td5.removeEventListener('click', td5fun);
-    WinCheck();
-}
-
-function td6fun() {
-    MiddleLineHorizontal++;
-    LastLineVertical++;
-    td6.style.backgroundColor = 'yellow';
-    td6.removeEventListener('click', td6fun);
-    WinCheck();
-}
-
-function td7fun() {
-    FirstLineVertical++;
-    LastLineHorizontal++;
-    DiagonalRightToLeft++;
-    td7.style.backgroundColor = 'yellow';
-    td7.removeEventListener('click', td7fun);
-    WinCheck();
-}
-
-function td8fun() {
-    LastLineHorizontal++;
-    MiddleLineVertical++;
-    td8.style.backgroundColor = 'yellow';
-    td8.removeEventListener('click', td8fun);
-    WinCheck();
-}
-
-function td9fun() {
-    LastLineHorizontal++;
-    LastLineVertical++;
-    DiagonalLeftToRight++;
-    td9.style.backgroundColor = 'yellow';
-    td9.removeEventListener('click', td9fun);
-    WinCheck();
-}
-
-function WinCheck() {
-    console.log(DiagonalLeftToRight);
-    console.log(DiagonalRightToLeft);
-    console.log(FirstLineVertical);
-    console.log(FirstLineHorizontal);
-    console.log(MiddleLineVertical);
-    console.log(MiddleLineHorizontal);
-    console.log(LastLineHorizontal);
-    console.log(LastLineVertical);
-
-    if((DiagonalLeftToRight == 3)||(DiagonalRightToLeft == 3)||(FirstLineVertical == 3)||(FirstLineHorizontal == 3)||(MiddleLineVertical == 3)||(MiddleLineHorizontal == 3)||(LastLineVertical == 3)||(LastLineHorizontal == 3)) {
-        window.alert('win');
+// @ტექსტის პროგრამულ ბრძანებად განხორციელება
+/*
+fToCode = new Function('alert("გაუმარჯოს")');
+function doSome() { return(fToCode()); }
+doSome();
+*/
+// @დინამიური ცვლადის შექმნა
+// console.log(window[part1+part2]);
+var starttime = performance.now();
+  for (var i = 1; i <= 9; i++) {
+    this["td" + i] = document.getElementById('td' + i);
+  }
+  
+  var TurnElement = document.getElementById('turn');
+  
+  for (let i = 1; i <= 9; i++) {
+    window['td' + i].addEventListener("click", function () {
+      testfun(i);
+    }, {
+      once: true
+    });
+    window['td' + i].style.cursor = 'pointer';
+  }
+  
+  var diagonal_l_to_r_x = 0;
+  var diagonal_r_to_l_x = 0;
+  
+  for (var i = 1; i <= 3; i++) {
+    this["row_" + i + '_x'] = 0;
+  }
+  
+  for (var i = 1; i <= 3; i++) {
+    this["col_" + i + '_x'] = 0;
+  }
+  
+  
+  var diagonal_l_to_r_o = 0;
+  var diagonal_r_to_l_o = 0;
+  
+  for (var i = 1; i <= 3; i++) {
+    this["row_" + i + '_o'] = 0;
+  }
+  
+  for (var i = 1; i <= 3; i++) {
+    this["col_" + i + '_o'] = 0;
+  }
+  
+  var Turn = 'x';
+  
+  function testfun(index) {
+    console.log(index);
+    window['td' + index].style.cursor = 'default';
+  
+    if (Turn == 'x') {
+      window['td' + index].innerHTML = '<img src="x.svg" class="image">';
+      testplus('x', index);
+      Turn = 'o';
+      TurnElement.innerHTML = 'Turn : O';
     } else {
-        return
+      window['td' + index].innerHTML = '<img src="o.svg" class="image">';
+      testplus('o', index);
+      Turn = 'x';
+      TurnElement.innerHTML = 'Turn : X';
     }
-}
+  
+  
+  }
+  
+  function testplus(turn, index) {
+    if (index == 1) {
+      window['diagonal_l_to_r_' + turn]++;
+      window['row_1_' + turn]++;
+      window['col_1_' + turn]++;
+    }
+    if (index == 2) {
+      window['row_1_' + turn]++;
+      window['col_2_' + turn]++;
+    }
+    if (index == 3) {
+      window['row_1_' + turn]++;
+      window['col_3_' + turn]++;
+      window['diagonal_r_to_l_' + turn]++;
+    }
+    if (index == 4) {
+      window['col_1_' + turn]++;
+      window['row_2_' + turn]++;
+    }
+    if (index == 5) {
+      window['col_2_' + turn]++;
+      window['row_2_' + turn]++;
+      window['diagonal_r_to_l_' + turn]++;
+      window['diagonal_l_to_r_' + turn]++;
+    }
+    if (index == 6) {
+      window['col_3_' + turn]++;
+      window['row_2_' + turn]++;
+    }
+    if (index == 7) {
+      window['col_1_' + turn]++;
+      window['row_3_' + turn]++;
+      window['diagonal_r_to_l_' + turn]++;
+    }
+    if (index == 8) {
+      window['row_3_' + turn]++;
+      window['col_2_' + turn]++;
+    }
+    if (index == 9) {
+      window['row_3_' + turn]++;
+      window['col_3_' + turn]++;
+      window['diagonal_l_to_r_' + turn]++;
+    }
+  
+    WinCheck();
+  }
+  
+  var firstrow = document.querySelectorAll('.firstrow');
+  var secondrow = document.querySelectorAll('.secondrow');
+  var thirdrow = document.querySelectorAll('.thirdrow');
+  
+  var firstcol = document.querySelectorAll('.firstcol');
+  var secondcol = document.querySelectorAll('.secondcol');
+  var thirdcol = document.querySelectorAll('.thirdcol');
+  
+  var diagonalrl = document.querySelectorAll('.diagonalrl');
+  var diagonallr = document.querySelectorAll('.diagonallr');
+  
+  var winnerwindow = document.getElementById('winner');
+  var winnertxt = document.getElementById('winnertxt');
+
+  function win(winner) {
+      winnerwindow.style.display = 'grid';
+      winnertxt.innerHTML = `Winner : ${winner}`; 
+      document.getElementById('tablewrap').innerHTML = '';
+      setTimeout(() => {
+      winnerwindow.style.left = '50%';
+      }, 500); 
+  }
+  
+  function WinCheck() {
+    if (diagonal_l_to_r_x == 3 || diagonal_l_to_r_o == 3) {
+      if (diagonal_l_to_r_x == 3) {
+        win('X');
+      } else {
+        win('O');
+      }
+      diagonallr.forEach(Element => {
+        Element.style.backgroundColor = 'red';
+      })
+    }
+  
+    if (diagonal_r_to_l_x == 3 || diagonal_r_to_l_o == 3) {
+      if (diagonal_r_to_l_x == 3) {
+        win('X');
+      } else {
+        win('O');
+      }
+      diagonalrl.forEach(Element => {
+        Element.style.backgroundColor = 'red';
+      })
+  
+    }
+  
+    if (row_1_x == 3 || row_1_o == 3) {
+      if (row_1_x == 3) {
+        win('X');
+      } else {
+        win('O');
+      }
+      firstrow.forEach(Element => {
+        Element.style.backgroundColor = 'red';
+      })
+  
+    }
+  
+    if (col_1_x == 3 || col_1_o == 3) {
+      if (col_1_x == 3) {
+        win('X');
+      } else {
+        win('O');
+      }
+      firstcol.forEach(Element => {
+        Element.style.backgroundColor = 'red';
+      })
+    }
+  
+    if (row_2_x == 3 || row_2_o == 3) {
+      if (row_2_x == 3) {
+        win('X');
+      } else {
+        win('O');
+      }
+      secondrow.forEach(Element => {
+        Element.style.backgroundColor = 'red';
+      })
+    }
+  
+    if (col_2_x == 3 || col_2_o == 3) {
+      if (col_2_x == 3) {
+        win('X');
+      } else {
+        win('O');
+      }
+      secondcol.forEach(Element => {
+        Element.style.backgroundColor = 'red';
+      })
+    }
+  
+    if (row_3_x == 3 || row_3_o == 3) {
+      if (row_3_x == 3) {
+        win('X');
+      } else {
+        win('O');
+      }
+      thirdrow.forEach(Element => {
+        Element.style.backgroundColor = 'red';
+      })
+    }
+
+    if (col_3_x == 3 || col_3_o == 3) {
+      if (col_3_x == 3) {
+        win('X');
+      } else {
+        win('O');
+      }
+      thirdcol.forEach(Element => {
+        Element.style.backgroundColor = 'red';
+      })
+    }
+  }
+
+  function res() {
+    window.location.reload();
+  }
+
+var endtime = performance.now();
+console.log(`${endtime - starttime} miliseconds`);
